0b456e866899edcec49f50340ab4e705c7cc6c9a
jaime luis <jrueda793@unab.edu.co>
2023-11-13 00:58:00 -0500

    SeccionPreguntas

---
+++ Assets/Scrips/ControllerData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControllerData : MonoBehaviour
+{ 
+	public void BottonIniciar()
+	{
+	    SceneManager.LoadScene(0);
+	}
+}
